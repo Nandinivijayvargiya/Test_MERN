b07379bc4eaea23b8bbb50c3b72019c524dfd08e
Nandinivijayvargiya <48726692+Nandinivijayvargiya@users.noreply.github.com>
2022-01-30 21:56:39 +0530

    Update Ques1.js

--- 30th Jan-Test/Ques1.js
+++ 30th Jan-Test/Ques1.js
@@ -1,20 +1,22 @@
 //Fibonacci Series
+//Fibonacci Series
 
   var n1=prompt("Enter first number");
     var n2=prompt("Enter second number");
       n1=parseInt(n1);
-      n2=parseInt(n2);
-        console.log(n1);
-        console.log(n2);
+      n2=parseInt(n2); 
+	  var fib_array=[];
+	  fib_array[0]=n1;
+	  fib_array[1]=n2;
    var count=prompt("Enter until what number want to print fibonacci series");
-      var n3;
+   count=parseInt(count);
+   
         for(i=2;i<=count;i++)
        {
-	      n3=n1+n2;
-	        console.log(n3);
-	         n1=n2;
-	           n2=n3;
+	      fib_array[i]=fib_array[i-2]+fib_array[i-1];
+		  console.log(fib_array[i]);
        }
+	   alert("The fibonacci number is:"+fib_array[count]);
 
 	
 	
